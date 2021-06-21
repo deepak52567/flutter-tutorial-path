5b28e31681011b1745c3e1de07d3f8cccd49f363
deepak-dell <deepak52567@gmail.com>
2021-06-22 01:54:38 +0530

    Add product remove feature using swipe dissmissable

--- apparel/lib/screens/cart_screen.dart
+++ apparel/lib/screens/cart_screen.dart
@@ -56,6 +56,7 @@ class CartScreen extends StatelessWidget {
                   cart.items.values.toList()[index].title,
                   cart.items.values.toList()[index].price,
                   cart.items.values.toList()[index].id,
+                  cart.items.keys.toList()[index],
                   cart.items.values.toList()[index].quantities),
             ),
           )
--- apparel/lib/widgets/cart_item.dart
+++ apparel/lib/widgets/cart_item.dart
@@ -1,31 +1,54 @@
+import 'package:apparel/providers/cart.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class CartItem extends StatelessWidget {
   final String id;
+  final String productId;
   final double price;
   final int quantity;
   final String title;
 
-  CartItem(this.title, this.price, this.id, this.quantity);
+  CartItem(this.title, this.price, this.id, this.productId, this.quantity);
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.symmetric(
-        horizontal: 15,
-        vertical: 4,
-      ),
-      child: Padding(
-        padding: EdgeInsets.all(8),
-        child: ListTile(
-          leading: CircleAvatar(
-            child: FittedBox(child: Text('\$${(price)}')),
+    return Dismissible(
+      child: Card(
+        margin: EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 4,
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: ListTile(
+            leading: CircleAvatar(
+              child: FittedBox(child: Text('\$${(price)}')),
+            ),
+            trailing: Text('${(quantity)} x'),
+            title: Text(title),
+            subtitle: Text('\$${(price * quantity)}'),
           ),
-          trailing: Text('${(quantity)} x'),
-          title: Text(title),
-          subtitle: Text('\$${(price * quantity)}'),
         ),
       ),
+      // add a unique key to differentiate
+      key: ValueKey(id),
+      background: Container(
+        color: Theme.of(context).errorColor,
+        child: Icon(
+          Icons.delete,
+          color: Colors.white,
+          size: 25,
+        ),
+        alignment: Alignment.centerRight,
+        padding: EdgeInsets.only(right: 20),
+        margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+      ),
+      direction: DismissDirection.endToStart,
+      onDismissed: (direction) {
+        // removes item based on its key
+        Provider.of<Cart>(context, listen: false).removeItem(productId);
+      },
     );
   }
 }
