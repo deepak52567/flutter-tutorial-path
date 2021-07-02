ca95ed6f39ab4e11381f93de8c8956850c634e61
deepak-dell <deepak52567@gmail.com>
2021-07-03 02:20:27 +0530

    Add pull to refresh widget and method implementation

--- apparel/lib/screens/user_products_screen.dart
+++ apparel/lib/screens/user_products_screen.dart
@@ -10,6 +10,10 @@ class UserProductsScreen extends StatelessWidget {
 
   const UserProductsScreen({Key? key}) : super(key: key);
 
+  Future<void> _refreshProducts(BuildContext context) async {
+    await Provider.of<Products>(context, listen: false).fetchAndSetProduct();
+  }
+
   @override
   Widget build(BuildContext context) {
     final productsData = Provider.of<Products>(context);
@@ -27,20 +31,25 @@ class UserProductsScreen extends StatelessWidget {
         ],
       ),
       drawer: AppDrawer(),
-      body: Padding(
-        padding: EdgeInsets.all(8.0),
-        child: ListView.builder(
-          itemCount: productsData.items.length,
-          itemBuilder: (_, index) => Column(
-            children: [
-              UserProductItem(
-                key: ValueKey(productsData.items[index].id + DateTime.now().toString()),
-                id: productsData.items[index].id,
-                title: productsData.items[index].title,
-                image: productsData.items[index].imageUrl,
-              ),
-              Divider(),
-            ],
+      //
+      body: RefreshIndicator(
+        onRefresh: () => _refreshProducts(context),
+        child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: ListView.builder(
+            itemCount: productsData.items.length,
+            itemBuilder: (_, index) => Column(
+              children: [
+                UserProductItem(
+                  key: ValueKey(
+                      productsData.items[index].id + DateTime.now().toString()),
+                  id: productsData.items[index].id,
+                  title: productsData.items[index].title,
+                  image: productsData.items[index].imageUrl,
+                ),
+                Divider(),
+              ],
+            ),
           ),
         ),
       ),
